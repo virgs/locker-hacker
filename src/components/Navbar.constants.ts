0bfb0b5f15163cccc2a02fc56726d895c836ef93
virgs <guilherme.moraes@outlook.com>
2026-03-04 11:26:15 -0800

    Update PayPal donation link in Navbar.constants.ts

--- src/components/Navbar.constants.ts
+++ src/components/Navbar.constants.ts
@@ -1,3 +1,3 @@
 export const GITHUB_URL = "https://github.com/virgs/locker-hacker";
-export const PAYPAL_URL = "https://www.paypal.com/donate/?hosted_button_id=YOUR_BUTTON_ID";
+export const PAYPAL_URL = "https://www.paypal.com/donate/?hosted_button_id=9C66XCH4VAX6U";
 export const APP_TITLE  = "Locker Hacker";
